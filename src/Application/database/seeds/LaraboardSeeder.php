5777f149a49c1bbcafb431db307d51ad52f48689
inlee <einable@gmail.com>
2020-05-02 20:17:55 +0900

    Modify seeder
    - For parent comment insertion

--- src/Application/database/seeds/LaraboardSeeder.php
+++ src/Application/database/seeds/LaraboardSeeder.php
@@ -12,6 +12,7 @@ class LaraboardSeeder extends Seeder
     public function run()
     {
         // 게시판 사용자, 게시판, 게시글, 댓글 생성
-        factory(App\Laraboard\Comment::class, 300)->create();
+        factory(App\Laraboard\Comment::class, 200)->create();
+        factory(App\Laraboard\Comment::class, 100)->create();
     }
 }
