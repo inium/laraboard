71bbc2bc8acd098172c9b7b9d6965a59cd0ccdff
inlee <einable@gmail.com>
2020-05-06 02:11:54 +0900

    Delete comments

--- src/Application/database/factories/LaraboardPrivilegeFactory.php
+++ src/Application/database/factories/LaraboardPrivilegeFactory.php
@@ -6,12 +6,8 @@ use App\Laraboard\Privilege;
 use Faker\Generator as Faker;
 
 $factory->define(Privilege::class, function (Faker $faker) {
-    // 한글 설명을 위한 faker 생성
-    // $fakerKr = \Faker\Factory::create('ko_KR');
-
     return [
         'name' => $faker->unique()->word,
-        // 'description' => $fakerKr->realText(),
         'description' => $faker->text,
         'is_admin' => $faker->boolean(3)
     ];
