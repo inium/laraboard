db14dee69d7d1d34f312a8981433436be76a235c
inlee <einable@gmail.com>
2020-06-06 20:54:31 +0900

    Add a pagination component

--- src/Resources/js/components/shared/PaginationComponent.vue
+++ src/Resources/js/components/shared/PaginationComponent.vue
@@ -4,24 +4,24 @@
         nav
             ul.pagination.mb-0
 
-                //- Create the move to first button if current is not the first.
+                //- Move to first if current is not the first.
                 li.page-item(v-if="!onFirstPage")
                     a.page-link(:href="firstPageUrl") &laquo;
 
-                //- Create the move to previous button if current is not the first.
+                //- Move to previous if there are more previous items.
                 li.page-item(v-if="!onFirstPage")
                     a.page-link(:href="prevPageUrl") &lsaquo;
 
-                //- Create page number and links.
+                //- Page number and links.
                 li.page-item(v-for="elem in elements"
                              :class="{ active: elem.active }")
                     a.page-link(:href="elem.link") {{ elem.page }}
 
-                //- Create the move to next button if current is not the last.
+                //- Move to next if there are more next items.
                 li.page-item(v-if="hasMorePages")
                     a.page-link(:href="nextPageUrl") &rsaquo;
 
-                //- Creat the move to last if current is not the last.
+                //- Move to last if current is not the last.
                 li.page-item(v-if="!onLastPage")
                     a.page-link(:href="lastPageUrl") &raquo;
 
@@ -30,22 +30,39 @@
 <script>
 export default {
     props: {
-        total: Number,
-        perPage: Number,
-        currentPage: Number,
-        lastPage: Number,
-        path: String,
+        currentPage: Number, // Current page number.
+        lastPage: Number, // Last page number.
+        basePath: String, // Pagination base path.
 
-        queryPageName: String,
+        // Pagination page query string name. Default is page.
+        // ex) lorem.ipsum?page=3   => queryPageName: page
+        queryPageName: {
+            type: String,
+            default: 'page'
+        },
 
-        numOfPageLink: {
+        // # of Pagination Elements. Default is 10.
+        numOfElements: {
             type: Number,
             default: 10
+        },
+
+        // query strings (except queryPageName)
+        queryParams: {
+            type: Object,
+            default: null
         }
     },
+    data() {
+        return {
+            // First page number.
+            firstPage: 1
+        };
+    },
     computed: {
+
         /**
-         * Determine if there are enough items to split into multiple pages.
+         * Determins if there are pagination items.
          *
          * @return bool
          */
@@ -54,7 +71,7 @@ export default {
         },
 
         /**
-         * Determine if there are more items in the data source.
+         * Determine if there are more items.
          *
          * @return bool
          */
@@ -63,7 +80,7 @@ export default {
         },
 
         /**
-         * Determine if the paginator is on the first page.
+         * Determine if current is on the first page.
          *
          * @return bool
          */
@@ -72,43 +89,93 @@ export default {
         },
 
         /**
-         * Determine if the paginator is on the last page.
-         *
+         * Determine current is on the last page.
+         * 
          * @return bool
          */
         onLastPage() {
             return this.currentPage >= this.lastPage;
         },
 
+        /**
+         * Get the first page url.
+         * 
+         * @return string
+         */
         firstPageUrl() {
-            return this.paginationUrl(this.path);
+            return this.paginationUrl(this.firstPage);
         },
+
+        /**
+         * Get the last page url.
+         * 
+         * @return string
+         */
         lastPageUrl() {
-            return this.paginationUrl(this.path, this.lastPage);
+            return this.paginationUrl(this.lastPage);
         },
+
+        /**
+         * Get the previous page url.
+         * 
+         * @return string
+         */
         prevPageUrl() {
-            let prevPage = this.currentPage - 1;
+            let prevPage = this.prevPage;
             if (prevPage < 0) {
                 prevPage = 0;
             }
 
-            return this.paginationUrl(this.path, prevPage);
+            return this.paginationUrl(prevPage);
         },
+
+        /**
+         * Get the next page url.
+         * 
+         * @return string
+         */
         nextPageUrl() {
-            let nextPage = this.currentPage + 1;
+            let nextPage = this.nextPage;
             if (nextPage > this.lastPage) {
                 nextPage = this.lastPage;
             }
 
-            return this.paginationUrl(this.path, nextPage);
+            return this.paginationUrl(nextPage);
         },
 
-        elements() {
-            let pageOffset 
-                    = Math.floor((this.currentPage - 1) / this.numOfPageLink);
+        /**
+         * Get the preivous page number
+         * 
+         * @return int
+         */
+        prevPage() {
+            return this.currentPage - 1;
+        },
 
-            let startPage = (pageOffset * this.numOfPageLink) + 1;
-            let endPage = (startPage - 1) + this.numOfPageLink;
+        /**
+         * Get the next page number
+         * 
+         * @return int
+         */
+        nextPage() {
+            return this.currentPage + 1;
+        },
+
+        /**
+         * Get the pagination page elements.
+         * It will return array of pagination info each consist of
+         * {page, link, active}.
+         * 
+         * - page: Page number.
+         * - link: Pagination Link.
+         * - active: If current page number true else otherwise.
+         * 
+         * @return array
+         */
+        elements() {
+            const pageOffset = (this.currentPage - 1) / this.numOfElements;
+            let startPage = (Math.floor(pageOffset) * this.numOfElements) + 1;
+            let endPage = (startPage - 1) + this.numOfElements;
 
             if (endPage > this.lastPage) {
                 endPage = this.lastPage;
@@ -118,7 +185,7 @@ export default {
             for (let page = startPage; page <= endPage; page++) {
                 pages.push({
                     'page': page,
-                    'link': this.paginationUrl(this.path, page),
+                    'link': this.paginationUrl(page),
                     'active': (this.currentPage == page) ? true : false
                 });
             }
@@ -127,15 +194,43 @@ export default {
         }
     },
     methods: {
-        paginationUrl(baseUrl, page = 0) {
-            let url = baseUrl;
-            if (page > 0) {
-                url = `${url}?${this.queryPageName}=${page}`
+
+        /**
+         * Get pagination url.
+         * 
+         * @param int page      A page number.
+         * @return string
+         */
+        paginationUrl(page = 0) {
+            let url = this.basePath;
+            let param = this.queryParams || [];
+
+            if (page > this.firstPage) {
+                param[this.queryPageName] = page;
             }
 
+            const queryString = this.httpBuildQuery(param);
+
+            url = `${url}?${queryString}`;
+
             return url;
+        },
+
+        /**
+         * PHP's 'http_build_query' function equivalent in javascript
+         *
+         * @param {*} jsonObj   JSON Object
+         * @see https://gist.github.com/luk-/2722097
+         */
+        httpBuildQuery(jsonObj) {
+            const keys = Object.keys(jsonObj);
+            const values = keys.map(key => jsonObj[key]);
+
+            return keys.map((key, index) => {
+                    return `${key}=${values[index]}`;
+                })
+                .join("&");
         }
-        // changed()
     }
 };
 </script>
