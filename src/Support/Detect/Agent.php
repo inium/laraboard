23101331fd6bcd7b555267b3cd828428bbf8c065
inlee <einable@gmail.com>
2020-06-06 20:52:47 +0900

    Modify a indent, comment, and change a return type

--- src/Support/Detect/Agent.php
+++ src/Support/Detect/Agent.php
@@ -8,7 +8,7 @@ class Agent
      * User Agent를 분석한 결과를 반환한다.
      *
      * @param string $agent     User Agent String
-     * @return array            분석결과. 아래의 항목들을 배열로 반환.
+     * @return object           분석결과. 아래의 항목들을 반환.
      *                          - agent: User Agent String
      *                          - device_type: 사용자 접속기기 형태.
      *                                  desktop, tablet, mobile, others 중 1.
@@ -21,7 +21,7 @@ class Agent
      *                          - browser_version: 사용자 접속 브라우저 버전.
      *                                          없거나 분석 불가할 경우 null.
      */
-    public function parse(string $agent): array
+    public function parse(string $agent): object
     {
         $ua = new \Jenssegers\Agent\Agent();
         $ua->setUserAgent($agent);
@@ -34,9 +34,9 @@ class Agent
         if      ($ua->isDesktop())    {   $deviceType = 'desktop';    }
         else if ($ua->isTablet())     {   $deviceType = 'tablet';     }
         else if ($ua->isMobile())     {   $deviceType = 'mobile';     }
-        else                            {   $deviceType = 'others';     }
+        else                          {   $deviceType = 'others';     }
 
-        return [
+        return (object)[
             'agent' => $agent,
             'device_type' => $deviceType,
             'os_name' => $platform ?: null,
