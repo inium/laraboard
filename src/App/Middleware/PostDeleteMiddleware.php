d7f00dbddeed8dd5a1e7d0c89710f238f4bd733b
inlee <einable@gmail.com>
2020-06-24 18:40:24 +0900

    Middleware 추가, 수정

--- src/App/Middleware/PostDeleteMiddleware.php
+++ src/App/Middleware/PostDeleteMiddleware.php
@@ -5,11 +5,14 @@ namespace Inium\Laraboard\App\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Inium\Laraboard\App\Board;
-use Inium\Laraboard\App\User;
+use Illuminate\Support\Facades\Session;
+use Inium\Laraboard\App\Board\BoardUserRoles;
+use Inium\Laraboard\App\Board\PostTrait;
 
 class PostDeleteMiddleware
 {
+    use PostTrait;
+
     /**
      * Handle an incoming request.
      *
@@ -19,52 +22,28 @@ class PostDeleteMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        /**
-         * 게시글 삭제관련 사용자 확인 절차
-         * 
-         * 1. 사용자 로그인 확인
-         *      - 로그인 하지 않았을 경우, 401 Unauthorized.
-         * 2. 게시판 정보가 존재하는지 확인
-         *      - 게시판 정보가 졵재하지 않을 경우, 404 Not found.
-         * 3. 게시글 존재 확인
-         *      - 게시글이 없으면 404 Not found.
-         *      - 게시글에 댓글이 존재하면 게시글 보기 페이지로 이동.
-         * 4. 게시글 사용자가 본인인지 확인
-         *      - 게시글이 본인이 작성한 글이 맞을 경우, next
-         *      - 게시글이 본인이 작성한 글이 아닐 경우, 401 Unauthorized
-         */
-
-        // 사용자가 로그인 하지 않았을 경우 401 예외 발생.
-        abort_if(!Auth::check(), 401, 'Invalid auth check.');
-
-        // 게시판 정보 Get. 없으면 404 예외 발생.
-        $board = Board::findByName($request->boardName);
-        abort_if(!$board, 404, 'Board not found.');
-
-        // 게시글 정보가 존재하지 않으면 404 예외 발생.
-        $post = $board->posts()->find($request->id);
-        abort_if(!$post, 404, 'Post not found.');
-
-        // 댓글 정보가 존재할 경우 삭제 불가.
-        if ($post->comments()->count() > 0) {
-
-            $redirectMessage = '댓글이 있을경우 삭제할 수 없습니다.';
-
-            return redirect()->route('laraboard.post.view', [
-                                'boardName' => $request->boardName,
-                                'id' => $request->id,
-                                'type' => $request->query('type', null),
-                                'query' => $request->query('query', null),
-                                'page' => $request->query('page', null)
-                            ])->with('message', $redirectMessage);
+        // 사용자가 로그인 하지 않았을 경우 로그인 페이지로 Redirect
+        if (!Auth::check()) {
+            return redirect()->route('login');
         }
 
-        // 게시글 작성 사용자 확인
-        if ($post->user()->id == Auth::id()) {
-            return $next($request);
+        // 게시글 확인. 없을 경우, 404 출력.
+        $post = $this->getPost($request->boardName, $request->id, false);
+        abort_if(!$post, 404, 'Can\'t find a posts.');
+        abort_if($post->user->user->id !== Auth::id(), 401, 'Unauthorized');
+
+        // 댓글이 있는 경우 삭제 불가
+        if ($post->comments->count() > 0) {
+            Session::flash('message', '댓글이 있는 글은 삭제할 수 없습니다.');
+            Session::flash('alert-class', 'alert-danger');
+
+            return redirect()->route('board.post.view', [
+                'boardName' => $request->boardName,
+                'id' => $request->id,
+                'page' => $request->get('page', null)
+            ]);
         }
-        else {
-            abort(401, 'Invalid post owner.');
-        }
+
+        return $next($request);
     }
 }
---
+++ src/App/Middleware/PostListSearchMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Inium\Laraboard\App\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inium\Laraboard\App\Board\BoardUserRoles;
+
+class PostListSearchMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\http\request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            // 게시판 사용자 권한 Get
+            $boardUserRoles = BoardUserRoles::roles($request->boardName);
+
+            // 게시글을 읽을 수 없을 경우
+            if (!$boardUserRoles->post->canRead) {
+
+                // 사용자가 로그인 하지 않았을 경우 로그인 페이지로 Redirect
+                if (!Auth::check()) {
+                    return redirect()->route('login');
+                }
+
+                // 그 외: 게시글 읽기 권한 없음.
+                throw new \Exception('Can\'t read board posts.', 401);
+            }
+
+            return $next($request);
+        }
+        catch (\Exception $e) {
+            abort($e->getCode(), $e->getMessage());
+        }
+    }
+}
