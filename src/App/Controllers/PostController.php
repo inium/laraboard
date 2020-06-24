f4ac00e0db4a01fb203b02588d671bc01a4382e5
inlee <einable@gmail.com>
2020-06-24 18:40:31 +0900

    Controller 추가, 수정

--- src/App/Controllers/PostController.php
+++ src/App/Controllers/PostController.php
@@ -3,18 +3,18 @@ namespace Inium\Laraboard\App\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Inium\Laraboard\App\Board\BoardUserRolesTrait;
-// use Inium\Laraboard\App\Board\PostListTrait;
-use Inium\Laraboard\App\Board\PostsTrait;
+use Illuminate\Support\Facades\Session;
+use Inium\Laraboard\App\Board\BoardUserRoles;
+use Inium\Laraboard\App\Board\CommentTrait;
 use Inium\Laraboard\App\Board\PostTrait;
-// use Inium\Laraboard\App\Board\SearchTrait;
-// use Inium\Laraboard\App\Board\RouteTrait;
+use Inium\Laraboard\App\Board\SearchTrait;
 use Inium\Laraboard\App\Board\RenderTemplateTrait;
-use Inium\Laraboard\App\Middleware\PostMiddleware;
+use Inium\Laraboard\App\Middleware\PostViewMiddleware;
+use Inium\Laraboard\App\Middleware\PostDeleteMiddleware;
 
 class PostController extends Controller
 {
-    use PostsTrait, PostTrait;
+    use CommentTrait, PostTrait, SearchTrait, RenderTemplateTrait;
 
     /**
      * Create a new controller instance.
@@ -23,7 +23,11 @@ class PostController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(PostMiddleware::class)->only('index');
+        // 게시글 보기 미들웨어
+        $this->middleware(PostViewMiddleware::class)->only('view');
+
+        // 게시글 삭제 미들웨어
+        $this->middleware(PostDeleteMiddleware::class)->only('delete');
     }
 
     /**
@@ -45,85 +49,82 @@ class PostController extends Controller
      * @param string $boardName 게시판 이름
      * @param integer $id       게시글 ID
      */
-    public function get(Request $request, string $boardName, int $id)
+    public function view(Request $request, string $boardName, int $id)
     {
         $query = $request->query('query', null); // 검색어
         $page = $request->query('page', 1);      // 페이지 번호
 
-        dd('qwer');
+        $isPostOwner = null;
 
-    //     // $temp= $this->isPostOwner($boardName, $id);
-    //     // dd($temp);
+        $post = $this->getPost($boardName, $id);
 
-    //     $viewParams = [
-    //         'post' => $this->post($boardName, $id),
-    //         'list' => $this->listOrSearch($boardName, $query, $page)
-    //     ];
+        $params = [
+            'post'     => $post,
+            'comments' => $this->getComments($boardName, $id),
+            'list'     => $this->listOrSearch($boardName, $page, $query),
+            'query'    => $query,
+            'page'     => $page,
+            'role'     => BoardUserRoles::roles($boardName),
+        ];
 
-    //     dd($viewParams);
-
-    //     // return $this->render('post', $viewParams);
+        return $this->render('post', $params);
     }
 
-    public function delete(string $boardName, int $id)
+    /**
+     * 게시글 삭제
+     * 
+     * -------------------------------------------------------------------------
+     * DELETE [/{$prefix}]/board/{boardName}/{id}
+     * 
+     * Route params
+     * @param string $prefix    Route Prefix. 환경설정(laraboard.php) 참조.
+     * @param string boardName  게시판 영문 이름.
+     * @param integer id        게시글 ID.
+     * -------------------------------------------------------------------------
+     *
+     * @param Request $request  Request
+     * @param string boardName  게시판 이름
+     * @param integer id        게시글 ID
+     */
+    public function delete(Request $request, string $boardName, int $id)
     {
+        $post = $this->getPost($boardName, $id);
+        $ret = $this->deletePostById($id);
 
+        // 게시글 삭제정보
+        $flashMessage = "게시글 \"{$post->subject}\" 이(가) 삭제되었습니다.";
+
+        Session::flash('message', $flashMessage); 
+        Session::flash('alert-class', 'alert-danger');
+
+        return redirect()->route('board.postListSearch.view', [
+            'boardName' => $boardName
+        ]);
     }
 
-    // /**
-    //  * 게시글 목록 혹은 검색 결과를 반환한다.
-    //  *
-    //  * @param string $boardName     게시판 이름
-    //  * @param string $query         검색어
-    //  * @param integer $page         페이지 번호
-    //  * @return array
-    //  */
-    // private function listOrSearch(string $boardName,
-    //                               string $query = null,
-    //                               int $page = 1): array
-    // {
-    //     $params = null;
 
-    //     // 검색어가 존재할 경우, 검색결과 반환
-    //     if ($query) {
-    //         $params = $this->search($boardName, $query, $page);
-    //     }
-    //     // 게시글 목록 반환
-    //     else {
-    //         $params = $this->postList($boardName, $page);
-    //     }
+    /**
+     * 게시글 목록 or 검색 결과 목록을 가져온다.
+     *
+     * @param string $boardName     게시판 름름
+     * @param integer $page         페이지 번호
+     * @param string $query         검색어
+     * @return array
+     */
+    private function listOrSearch(string $boardName,
+                                  int $page = 1,
+                                  string $query = null): array
+    {
+        // Get 검색 결과 or 게시글 목록
+        $params = $query ? $this->getSearchResult($boardName, $query, $page) :
+                           $this->getPostList($boardName, $page);
 
-    //     $viewParams = $this->viewListSearchParams($boardName, $query, $page);
+        $viewParams = [
+            'role'  => BoardUserRoles::roles($boardName),
+            'query' => $query,
+            'page'  => $page
+        ];
 
-    //     return array_merge($params, $viewParams);
-    // }
-
-    // /**
-    //  * View에 사용할 사용자 Role에 따른 Route 정보, 검색 form 정보를 반환한다.
-    //  *
-    //  * @param string $boardName     게시판 이름
-    //  * @param string $query         검색어
-    //  * @param int $page             게시글 페이지 번호
-    //  * @return array
-    //  */
-    // private function viewListSearchParams(string $boardName,
-    //                                       string $query = null,
-    //                                       int $page = 1
-    //                                       ): array
-    // {
-    //     $boardUserRoles = $this->boardUserRoles($boardName);
-    //     $canWrite = $boardUserRoles->post->canWrite;
-
-    //     $routes = [
-    //         'list' => $this->routeListSearch($boardName, $page, $query),
-    //         'write' => $canWrite ? $this->routePostWrite($boardName) : null,
-    //         'modify' => null,
-    //         'delete' => null
-    //     ];
-
-    //     return [
-    //         'routes' => $routes,
-    //         'searchForm' => $this->searchForm($boardName)// 로그인 사용자만 가능
-    //     ];
-    // }
+        return array_merge($params, $viewParams);
+    }
 }
