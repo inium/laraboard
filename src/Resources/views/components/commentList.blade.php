769949362037eed6f2da4f287801c0939944a679
inlee <einable@gmail.com>
2020-06-28 01:29:34 +0900

    Quill Editor Viewer 적용

--- src/Resources/views/components/commentList.blade.php
+++ src/Resources/views/components/commentList.blade.php
@@ -28,6 +28,11 @@
 
     </div>
 
+    {{-- 댓글 쓰기 Form --}}
+    <div class="py-2">
+        {{-- <form action="" id="formComment"></form> --}}
+    </div>
+
     {{-- 댓글 목록 --}}
     <div class="lb-comments-contents py-2">
 
@@ -35,8 +40,9 @@
 
             <div class="media mb-3 @if($comment['parent_comment_id']) child @endif">
 
-                {{-- 게시글 썸네일 --}}
+                {{-- 댓글 사용자 썸네일 --}}
                 @include('laraboard::components.shared.thumbnail', [
+                    'thumbnail' => $comment['user']['thumbnail_path'],
                     'alt' => 'thumbnail',
                     'class' => 'rounded-circle thumbnail align-self-start mr-3'
                 ])
@@ -45,6 +51,8 @@
                     <div class="d-flex jusityf-content-between">
                         <div>
                             <ul class="list-inline">
+
+                                {{-- 댓글 작성자 --}}
                                 <li class="list-inline-item">
                                     <strong>
 
@@ -57,9 +65,9 @@
                                     </strong>
                                 </li>
 
+                                {{-- 댓글 작성날짜 --}}
                                 <li class="list-inline-item">
 
-                                    {{-- Carbon locale to ko, diff for humans --}}
                                     @include('laraboard::components.shared.carbonDate', [
                                         'date' => $comment['created_at']
                                     ])
@@ -71,11 +79,20 @@
 
                     <div>
 
+                        {{-- 댓글 --}}
+                        <div id="comment_{{$comment['id']}}"
+                             class="comment"
+                             data-id="{{ $comment['id'] }}">
+
+                            {!! htmlspecialchars_decode($comment['content']) !!}
+
+                        </div>
+
                         {{-- 검색어 mark. 없을 경우 일반 문자열 출력. --}}
-                        @include('laraboard::components.shared.mark', [
+                        {{-- @include('laraboard::components.shared.mark', [
                             'query' => $query,
                             'content' => $comment['content']
-                        ])
+                        ]) --}}
 
                     </div>
                 </div>
@@ -132,7 +149,21 @@
 @push('scripts')
     <script>
         $(document).ready(function () {
-            
+
+            // 댓글 Quill Editor Post viewer 생성
+            $('.comment').each(function() {
+                let q = new Quill('#' + $(this).attr('id'), {
+                    readOnly: true,
+                    theme: 'snow'
+                });
+            });
+
+            // // Quill Editor Post viewer 생성
+            // const quillPost = new Quill('#viewer', {
+            //     readOnly: true,
+            //     theme: 'snow'
+            // });
+
         });
     </script>
 @endpush
